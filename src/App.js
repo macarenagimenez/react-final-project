7238ad255088a8f3ad8338571a7ecce47dc318f0
macarenagimenez <107077871+macarenagimenez@users.noreply.github.com>
2022-09-19 16:28:58 -0300

    CSS STRUCTURE

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="conteiner">
-        <Header />
-      </div>
+      <Header />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWind } from "@fortawesome/free-solid-svg-icons";
 import { faCloudRain } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarDay } from "@fortawesome/free-solid-svg-icons";
 
 import { Container } from "react-bootstrap";
 import { Row } from "react-bootstrap";
@@ -11,36 +13,60 @@ import "./Header.css";
 
 export default function Header() {
   return (
-    <div className="header">
-      <Container>
-        <Row>
-          <Col className="date">
-            <p>OCT 21</p>
-            <p>London</p>
-          </Col>
-          <Col className="searchEngine">
-            <form>
-              <input type="search" placeholder="Type your city..." />
-              <button>Search</button>
-            </form>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="description">Cloudy (icon) </div>
-            <span className="temperature">35</span>
-            <span>°C | °F</span>
-          </Col>
-          <Col>
-            <div className="windy">
-              <FontAwesomeIcon icon={faWind} /> Wind 10km/h
-            </div>
-            <div className="humidity">
-              <FontAwesomeIcon icon={faCloudRain} /> Humidity 10%
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <body>
+      <div className="header">
+        <Container>
+          <Row>
+            <Col xs={3} className="city">
+              <p>LONDON</p>
+            </Col>{" "}
+            <Col></Col>
+            <Col xs={5} className="searchEngine">
+              <form>
+                <input type="search" placeholder="Type your city..." />
+                <button>
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+              </form>
+            </Col>
+          </Row>
+          <Row>
+            <Col></Col>
+            <Col xs={11}>
+              <p className="textMessage">It's f*cking raining</p>
+            </Col>
+          </Row>
+          <div className="infoWeather">
+            <Row>
+              <Col>
+                <div className="description">
+                  <img
+                    src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+                    alt="icon"
+                  />
+                  <span>Cloudy</span>
+                </div>
+                <span className="temperature">35</span>
+                <span className="degrees">°C | °F</span>
+              </Col>
+              <Col></Col>
+              <Col>
+                <div className="parameters">
+                  <div className="day">
+                    <FontAwesomeIcon icon={faCalendarDay} /> Today: Wed 10:30hs
+                  </div>
+                  <div className="windy">
+                    <FontAwesomeIcon icon={faWind} /> Wind:10km/h
+                  </div>
+                  <div className="humidity">
+                    <FontAwesomeIcon icon={faCloudRain} /> Humidity: 10%
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div>
+    </body>
   );
 }
