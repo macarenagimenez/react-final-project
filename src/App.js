eaef99e0b2217ec8017bb5bbe68b5d130bfd7bc8
macarenagimenez <107077871+macarenagimenez@users.noreply.github.com>
2022-09-23 12:30:07 -0300

    add noreferrer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <span className="autor">
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/maria-macarena-gimenez"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Macu Gimenez
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/macarenagimenez/react-final-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </span>
     </div>
   );
 }
